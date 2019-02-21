f36202c5e04b4c1f16caa5a12ffce7bf21d1a45d
Diamondgal <michwray74@gmail.com>
2019-02-21 13:11:01 -0500

    To Do App Code updates

--- mySQLPDO/ToDoAppHTMLindex.php
+++ mySQLPDO/ToDoAppHTMLindex.php
@@ -1,71 +1,128 @@
 <!DOCTYPE HTML>
 <html lang="en">
-<head></head>
+<head>
+<link href="MainToDoApp.css" rel="Stylesheet">    
+</head>
 <body>
 <h1>Here's Your To Do App</h1>
-<form method="post">
+<form action = ""  method="post">
 To Do Item:<input type ="text" name="name" value = "">
 <input type="submit" value="AddToList">
-
+   <table class="table table-striped">
 </form>
 
 <h2>Current To Do's<h2>
-<table class="table table-striped">
-<tbody>
 
 <?php
+//  validate string
 
+if(!empty($_POST) && isset($_POST)) {
+    $name = $_POST['name'];
+    function validateName($name) {
+        return $name;
+    }
+    validateName($name);
+}
 $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "todoapp";
+echo"<table>";
 
-$pdo = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+// $pdo = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
 
-//Create a task and bind the record to tasks column, or delete a 
-//task from tasks on ID on done 'submit' 
+try {
+    $pdo = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
 
-if(isset($_POST['submit'])) {
-    $name = $_POST["name"];
-    $xyz = $pdo->prepare("INSERT INTO tasks (task) VALUES(:task)");
-    $xyz->bindValue(':task', $name, PDO::PARAM-STR);
-    $xyz->execute();
- }elseif(isset($_POST['done'])) {
-     $id = $_POST['id'];
-     $xyz = $pdo->prepare("delete from tasks where id = :id");
-     $xyz->bindValue(':id',$id, PDO::PARAM_INT);
-     $xyz->execute();
- }
-?>
+    // set the PDO error mode to exception
 
-<?php
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $xyz = "INSERT INTO tasks (task, status)
+        VALUES ('$name','pending')";
+    
+        // use exec() because no results are returned
 
-// select tasks from db, per row validate data, display records
+    $pdo->exec($xyz);
+    $last_id = $pdo->lastInsertID();
+
+
+    echo "New Record succesfully created";
+}
+catch(PDOExeption $e)
+{
+    echo $xyz . "<br>" . $e->getMessage();
+}
+
+$pdo = null;
+
+//Create a task and bind the task to tasks column, or delete a 
+//task from tasks on ID on done 'submit' in prepared statement 
+// select tasks from db, per row data, display records
 // in table 
-$xyz = $pdo->prepare("SELECT * FROM tasks ORDER by ID DESC");
-$xyz->execute();
-foreach($xyz as $row) {
+
+
+try {
+    
+    $pdo = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    
+    // set the "Select * to fetch data
+    $stmt =$pdo->prepare("SELECT * from tasks");
+    $stmt->execute();
+
+    $stmt->setFetchMode(PDO::FETCH_ASSOC);
+    $taskList = $stmt->fetchAll();
+
+    var_dump($taskList);
+
+    foreach ($taskList as $row) {
+     echo "<tr>";
+     echo "<td>" .$row['Task'] . "</td>";
+     echo "<td>" . "<form method='POST' action=''>" . "<button type ='submit' name='done'>Done</button></form>" .
+     "<input type='hidden' name='id' value='" . $row['id'] ."' ><input type='hidden' name='delete' value='true'> ";     
+    } 
+}
+
+catch(PDOExeption $e)
+{
+    echo "Error" . "<br>" . $e->getMessage();
+}
+
+    if(isset($_POST['submit'])) {
+        $name = $_POST['name'];
+        $xyz = $pdo->prepare("INSERT INTO tasks (Task) VALUES(:Task)");
+        $xyz->bindValue(':task', $name, PDO::PARAM_STR);
+        $xyz->execute();
+     }elseif(isset($_POST['done'])) {
+         $id = $_POST['id'];
+         $xyz = $pdo->prepare("delete from tasks where id = :id");
+         $xyz->bindValue(':id', $id, PDO::PARAM_INT);
+         $xyz->execute();
+     }
+
+     try {
+
+    $xyz = $pdo->prepare("SELECT * FROM tasks ORDER by (id) DESC");
+    $xyz->execute();
+    foreach($xyz as $row) {
+
 ?>
 
-<tr>
 
-    <td><?=htmlspecialchars($row['task'])?></td>
-    <td>
+    <tbody>
 
-<form method="POST">
-<input type ="submit" name="done">Done</button>
-<input type="hidden" name="id" value=<?=row['id']?>>
-<input type="hidden" name="delete" value="true">
-</form>
 
-</td>
-</tr>
 <?php
     }
+}
+    catch(PDOExeption $e)
+{
+    echo "message". "<br>" . $e->getMessage();
+}
+     
 ?>
 
-
-</table>
-</body>
-</html>
+ </table>
+ </body>
+ </html>
 
